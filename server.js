dac472eeeeeb1746d844887b34bf0aebb8254884
ROHIT1004 <rohitnarganwe@gmail.com>
2021-07-24 04:27:57 +0530

    reslove

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/orders/', ordersRoute);
 
 if(process.env.NODE_ENV === 'production'){
 
-  app.use('/' , express.static('client/build'))
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*' , (req ,res) =>{
 
@@ -34,6 +34,16 @@ if(process.env.NODE_ENV === 'production'){
 app.get("/", (req,res)=> {
   res.send("working  fine !@");
 })
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
 
 //app.use(express.urlencoded({extended: true}));
 //Db config
@@ -59,8 +69,4 @@ mongoose.connect("mongodb+srv://rajat:rajatpw@cluster0.1bdwh.mongodb.net/mern-pi
 		})
         .then(() => console.log("Mongodb is  connected"))
         .catch(err => console.log(err+"errors Mongodb connections"));
-
-
-const port = process.env.PORT || 5000;
-  
-app.listen(port, () => console.log(`Server started on port ${port}`)); 
+        
