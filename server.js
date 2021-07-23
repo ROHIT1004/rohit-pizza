d1f383b1cbcfcda3ac1c2f7c538a0a70025bfce3
ROHIT1004 <rohitnarganwe@gmail.com>
2021-07-24 03:54:45 +0530

    mongodb

--- server.js
+++ server.js
@@ -45,18 +45,21 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Mongodb is connected"))
     .catch(err => console.log(err+"error ouccessssssssssssds"));
-/*/
+  mongoose.connect("'mongodb://localhost:27017/mern-pizza", {
+
+    /*/
 //mongodb+srv://rajat:rajatpw@cluster0.1bdwh.mongodb.net/mern-pizza
 //'mongodb://localhost:27017/
 ///mongoose.connect("'mongodb://localhost:27017/mern-pizza", {
-////mongoose.connect("mongodb+srv://rajat:rajatpw@cluster0.1bdwh.mongodb.net/mern-pizza", {
-  mongoose.connect("'mongodb://localhost:27017/mern-pizza", {
+mongoose.connect("mongodb+srv://rajat:rajatpw@cluster0.1bdwh.mongodb.net/mern-pizza", {
       useUnifiedTopology: true,
 			useNewUrlParser: true,
 			useCreateIndex: true,
 		})
         .then(() => console.log("Mongodb is connected"))
         .catch(err => console.log(err+"error Mongodb connections"));
-        const port = process.env.PORT || 5000;
+
+
+const port = process.env.PORT || 5000;
   
 app.listen(port, () => console.log('Server started on port '+port)); 
