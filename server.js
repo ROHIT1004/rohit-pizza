08deff44e848a74d8375c1987b3d5fb5a993d8ff
ROHIT1004 <rohitnarganwe@gmail.com>
2021-07-24 04:31:52 +0530

    r

--- server.js
+++ server.js
@@ -25,20 +25,16 @@ if(process.env.NODE_ENV === 'production'){
 
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*' , (req ,res) =>{
 
-    res.sendFile(path.resolve(__dirname , 'client/build/intex.html'))
-
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
+  });
 }
 app.get("/", (req,res)=> {
   res.send("working  fine !@");
 })
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
